f9d79498c2a830dc5e02e075b65247b7ddd90bd2
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-11-14 14:29:50 +0500

    Task3

---
+++ Tyuiu.PuzyrevIV.Sprint4.Task3.V20.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.PuzyrevIV.Sprint4.Task3.V20.Lib
+{
+    public class DataService : ISprint4Task3V20
+    {
+        public int Calculate(int[,] array)
+        {
+            if (array == null || array.GetLength(0) == 0 || array.GetLength(1) == 0)
+            {
+                throw new ArgumentException("Входной массив не может быть пустым или null.");
+            }
+
+            int rows = array.GetLength(0);
+
+            int minElement = array[0, 0];
+            for (int i = 1; i < rows; i++)
+            {
+                int currentElement = array[i, 0];
+                if (currentElement < minElement)
+                {
+                    minElement = currentElement;
+                }
+            }
+
+            return minElement;
+        }
+    }
+}
---
+++ Tyuiu.PuzyrevIV.Sprint4.Task3.V20.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PuzyrevIV.Sprint4.Task3.V20.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint4.Task3.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { { 8, 7, 7, 8, 5 },
+                                          { 4, 3, 5, 3, 6 },
+                                          { 5, 3, 8, 6, 3 },
+                                          { 6, 3, 8, 5, 4 },
+                                          { 3, 6, 8, 3, 4 } };
+            int res = ds.Calculate(mas2);
+            int wait = 4;
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
